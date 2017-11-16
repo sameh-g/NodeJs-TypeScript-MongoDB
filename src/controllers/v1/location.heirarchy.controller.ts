d9f3abeed6dd16a43439382f986a43fcb8b646b8
ITWORX\sameh.george <sameh.george@itworx.com>
2017-11-16 16:55:19 +0200

    commit urls

--- src/controllers/v1/location.heirarchy.controller.ts
+++ src/controllers/v1/location.heirarchy.controller.ts
@@ -17,7 +17,7 @@ export class LocationHeirarchyController {
      * @param req 
      * @param res 
      */
-    public GetLocations(req: Request, res: Response) {
+public GetLocations(req: Request, res: Response) {
 
 client.get("sameh",function(error:any,obj:any)
     {
@@ -34,9 +34,47 @@ else
         res.json("My Locations") }, 10000);
 }
     })
- 
-        
-     
-        
     }
+    public getRegions(req: Request, res: Response) {
+        res.json("GetRegions.........")
+    }      
+    public getRegionByCode(req: Request, res: Response) {
+        res.json("getRegionByCode.........")
+    } 
+    public getTerritoryByRegionCode(req: Request, res: Response) {
+        res.json("getTerritoryByRegionCode.........")
+    }       
+    public getTerritoryByCode(req: Request, res: Response) {
+        res.json("getTerritoryByCode.........")
+    }   
+    public getCountries(req: Request, res: Response) {
+        res.json("getCountries.........")
+    } 
+    public getCountriesByRegionCode(req: Request, res: Response) {
+        res.json("getCountriesByRegionCode.........")
+    }   
+    public getCountriesByCountryCode(req: Request, res: Response) {
+        res.json("getCountriesByCountryCode.........")
+    }
+    public getCountryCenters(req: Request, res: Response) {
+        res.json("getCountryCenters.........")
+    }  
+    public getCountryCenter(req: Request, res: Response) {
+        res.json("getCountryCenter.........")
+    }  
+    public getSites(req: Request, res: Response) {
+        res.json("getSites.........")
+    }  
+    public getSitesByCode(req: Request, res: Response) {
+        res.json("getSitesByCode.........")
+    } 
+    public getSitesByCodeForSpicificDomain(req: Request, res: Response) {
+        res.json("getSitesByCodeForSpicificDomain.........")
+    } 
+    public getSitesCountry(req: Request, res: Response) {
+        res.json("getSitesCountry.........")
+    } 
+    public getSiteCountry(req: Request, res: Response) {
+        res.json("getSiteCountry.........")
+    }   
 }
